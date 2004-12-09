5cb527ccbb82dfb0982d0d2cf7de5df985c7271d
robbat2 <robbat2>
2004-12-09 14:01:34 +0000

    perl cleanup.

--- mkpasswd.pl
+++ mkpasswd.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
-
+# $Header: /code/convert/cvsroot/infrastructure/diradm/Attic/mkpasswd.pl,v 1.4 2004/12/09 14:01:34 robbat2 Exp $
 use Crypt::SmbHash qw(lmhash nthash ntlmgen);
+#use warnings;
+use strict;
 
 sub generatesalt {
     my $formatstr = $_[0];
@@ -15,9 +17,15 @@ sub generatesalt {
     return sprintf($formatstr, $randstr);
 }
 
-if( $ARGV[0] =~ /-m/ ) { print crypt( $ARGV[0], generatesalt( '$1$%.8s$' ) ) }
-elsif( $ARGV[0] =~ /-i/ ) { print crypt( $ARGV[0], generatesalt ) }
-elsif( $ARGV[0] =~ /-n/ ) { print nthash( $ARGV[1] ) }
-elsif( $ARGV[0] =~ /-l/ ) { print lmhash( $ARGV[1] ) }
+# trim whitespace
+my $passwd = $ARGV[1];
+$passwd =~ s/^\s+//g;
+$passwd =~ s/\s+$//g;
+#print "passwd: '",$passwd,"'\n";
+
+if( $ARGV[0] =~ /-m/ ) {    print  crypt( $passwd, generatesalt( '$1$%.8s$' ) ) }
+elsif( $ARGV[0] =~ /-i/ ) { print  crypt( $passwd, generatesalt ) }
+elsif( $ARGV[0] =~ /-n/ ) { print nthash( $passwd ) }
+elsif( $ARGV[0] =~ /-l/ ) { print lmhash( $passwd ) }
 
 #print "\n"
